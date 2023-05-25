7508d892e98feccdaeda5c9e1e556772ef99d19b
Thadeu Esteves Jr <tadeuu@gmail.com>
2023-05-25 12:19:39 -0300

    wip: create first version

---
+++ spec/index.test.ts
@@ -0,0 +1,139 @@
+import { describe, it, expect, vi, afterEach } from 'vitest'
+
+import AtendeSimples from '@/index'
+
+console.info = vi.fn()
+
+function createDiv() {
+  let div = document.createElement('div')
+  div.classList.add('phonenumber')
+  div.innerHTML = ''
+
+  document.body.appendChild(div)
+}
+
+function clearAllCookie() {
+  document.cookie.split(';').forEach(function (c) {
+    document.cookie = c.replace(/^ +/, '').replace(/=.*/, '=;expires=' + new Date().toUTCString() + ';path=/')
+  })
+}
+
+describe('AtendeSimples', () => {
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
+
+  describe('#googleClientId', () => {
+    it('should return a string', () => {
+      document.cookie = '_ga=GA1.1.553524416.1685026105;OptanonAlertBoxClosed=2023-02-01T15:12:04.164Z'
+
+      const callTracking = new AtendeSimples.CallTracking({
+        token: 'UaoPcchkelsSn0BIELN89',
+        html: {
+          event: 'load',
+          selector: '.phonenumber',
+        },
+      })
+
+      expect(callTracking.googleClientId()).toBe('553524416.1685026105')
+    })
+
+    it('should return a empty string', () => {
+      clearAllCookie()
+
+      const callTracking = new AtendeSimples.CallTracking({
+        token: 'UaoPcchkelsSn0BIELN89',
+        html: {
+          event: 'load',
+          selector: '.phonenumber',
+        },
+      })
+
+      expect(callTracking.googleClientId()).toBe('')
+    })
+  })
+
+  it('#run', async () => {
+    createDiv()
+
+    document.cookie = '_ga=GA1.1.553524416.1685026105;OptanonAlertBoxClosed=2023-02-01T15:12:04.164Z'
+
+    const callTracking = new AtendeSimples.CallTracking({
+      token: 'UaoPcchkelsSn0BIELN89',
+      html: {
+        event: 'load',
+        selector: '.phonenumber',
+      },
+    })
+
+    let result = await callTracking.run()
+
+    expect(result).toBeTruthy()
+    expect(document.querySelector('.phonenumber').textContent).toBe(result)
+  })
+
+  it('empty number', async () => {
+    createDiv()
+
+    const callTracking = new AtendeSimples.CallTracking({
+      token: 'some-token',
+      fallback: {
+        error: '1234',
+      },
+      html: {
+        event: 'load',
+        selector: '.phonenumber',
+      },
+    })
+
+    let result = await callTracking.run()
+
+    expect(result).toBeTruthy()
+    expect(document.querySelector('.phonenumber').textContent).toBe('1234')
+  })
+
+  it('empty cid on the body', async () => {
+    document.cookie = ''
+
+    createDiv()
+
+    const callTracking = new AtendeSimples.CallTracking({
+      token: 'some-token',
+      fallback: {
+        error: '1234',
+      },
+      html: {
+        event: 'load',
+        selector: '.phonenumber',
+      },
+    })
+
+    let result = await callTracking.run()
+
+    expect(result).toBeTruthy()
+    expect(document.querySelector('.phonenumber').textContent).toBe('1234')
+  })
+
+  it('when div doesnt exist', async () => {
+    document.querySelectorAll('.phonenumber').forEach((div) => div.remove())
+
+    const callTracking = new AtendeSimples.CallTracking({
+      token: 'UaoPcchkelsSn0BIELN89',
+      fallback: {
+        error: '1234',
+      },
+      html: {
+        event: 'load',
+        selector: '.phonenumber',
+      },
+    })
+
+    let result = await callTracking.run()
+
+    expect(console.info).toHaveBeenCalledWith('[CallTracking] Campo HTML não encontrado')
+
+    expect(result).toBeTruthy()
+    expect(result).toBe('1234')
+    expect(document.querySelector('.phonenumber')).toBeFalsy()
+  })
+})
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import * as dotenv from 'dotenv'
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+
+import fetch from 'axios'
+
+type CallTrackingProps = {
+  token: string
+  fallback?: any
+  html: {
+    event: string
+    selector: string
+  }
+}
+
+class CallTracking {
+  #token: any
+  #fallback: any
+  #html: any
+
+  constructor(props: CallTrackingProps) {
+    this.#token = props.token
+    this.#fallback = props.fallback
+    this.#html = props.html
+  }
+
+  googleClientId() {
+    let ga = document.cookie.split(';').find((o) => o.match(/_ga=(.+?)/))
+    if (!ga) return ''
+
+    return ga.split('.').slice(-2).join('.')
+  }
+
+  run() {
+    const url = `${process.env.CALL_TRACKING_URL}/2/${this.#token}`
+
+    const options = {
+      method: 'POST',
+      data: JSON.stringify({ cid: this.googleClientId() }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    let $el = document.querySelector(this.#html.selector)
+
+    if (!$el) {
+      console.info('[CallTracking] Campo HTML não encontrado')
+      return this.#fallback.error
+    }
+
+    return fetch(url, options).then((response) => {
+      const { data } = response
+
+      // update HTML ELement
+      $el.textContent = data?.number || this.#fallback.error
+
+      return data?.number || this.#fallback.error
+    })
+  }
+}
+
+const AtendeSimples = { CallTracking }
+
+export default AtendeSimples
